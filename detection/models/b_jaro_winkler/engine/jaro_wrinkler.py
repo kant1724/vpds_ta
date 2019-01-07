459982c59a708f660fbc78aea1912d93b1b21c95
kant1724 <sonseungbum83@gmail.com>
2019-01-07 15:11:51 +0900

    commit

--- detection/models/b_jaro_winkler/engine/jaro_wrinkler.py
+++ detection/models/b_jaro_winkler/engine/jaro_wrinkler.py
@@ -81,9 +81,11 @@ def new_jaro_wrinkler(ying, yang, voca_weight):
 
     ying_sub = 0
     yang_sub = 0
-    for i in range(len(yang)):
-        if voca_weight.get(yang[i], None) == None:
+    for i in range(len(ying)):
+        if voca_weight.get(ying[i], None) == None:            
             ying_sub += 1
+    for i in range(len(yang)):
+        if voca_weight.get(yang[i], None) == None:            
             yang_sub += 1
             
     # looking only within search range, count & flag matched pairs
